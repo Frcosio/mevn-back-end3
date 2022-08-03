ad431ca6493959e3f3d292e54375118e593e807f
Frcosio <83733844+Frcosio@users.noreply.github.com>
2022-08-03 11:24:38 -0500

    mevn backend v1

--- index.js
+++ index.js
@@ -3,30 +3,33 @@ import "./database/connectdb.js";
 import express from "express";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-import linkRouter from "./routes/link.route.js"
-import redirectRouter from "./routes/redirect.route.js"
-import cors from 'cors'
+import linkRouter from "./routes/link.route.js";
+import redirectRouter from "./routes/redirect.route.js";
+import cors from "cors";
 const app = express();
 
-const whiteList = [process.env.ORIGIN1, process.env.ORIGIN2]
+const whiteList = [process.env.ORIGIN1, process.env.ORIGIN2];
 
 app.use(
-    cors({
-        origin: function(origin, callback){
-            if(whiteList.includes(origin)){
-                return callback(null, origin)
-            }
-            return callback("Error de CORS origin:" + origin + "No autirizado!")
-        }
-        //origin: [process.env.ORIGIN1],
-}));
+  cors({
+    origin: function (origin, callback) {
+      console.log("😊😉 => ", origin);
+      if (!origin || whiteList.includes(origin)) {
+        return callback(null, origin);
+      }
+      return callback("Error de CORS origin:" + origin + "No autirizado!");
+    },
+    //origin: [process.env.ORIGIN1],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
 //ejemplo back redirect (opcional)
-app.use('/', redirectRouter)
+app.use("/", redirectRouter);
 app.use("/api/v1", authRouter);
-app.use("/api/v1/links", linkRouter)
+app.use("/api/v1/links", linkRouter);
 
 //solo para el ejemplo de login/token
 //app.use(express.static("public"));
